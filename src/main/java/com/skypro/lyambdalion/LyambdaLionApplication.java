740e7047ba1a7df8bdc68b71e1846e6d124b2f36
leush <leushkinml@mail.ru>
2023-01-04 23:51:29 +0300

    Initial commit

---
+++ src/main/java/com/skypro/lyambdalion/LyambdaLionApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.lyambdalion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyambdaLionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LyambdaLionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/lyambdalion/LyambdaLionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.lyambdalion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyambdaLionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
